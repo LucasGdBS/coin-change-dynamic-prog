a235181840bc9fde798d30e97be9e9a9974b543b
LucasGdbs <lgbs@cesar.school>
2025-05-08 17:50:20 -0300

    feat: implements CoinForm in main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import CoinForm from "./components/CoinForm";
+
 export default function App() {
-  return <h1 className="text-6xl text-sky-600">Texto</h1>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center space-y-30">
+      <h1 className="text-2xl font-semibold text-neutral-800">
+        Troco de Moedas
+      </h1>
+      <CoinForm />
+    </div>
+  );
 }
